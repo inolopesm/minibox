82f4548ab13bfae140636253a53ef8ca570330b1
Matheus Lopes <inolopesm@gmail.com>
2023-08-23 17:42:10 -0300

    remove @next/env

--- knexfile.js
+++ knexfile.js
@@ -1,8 +1,3 @@
-const { loadEnvConfig } = require("@next/env");
-
-const dev = process.env.NODE_ENV !== "production";
-const { combinedEnv: env } = loadEnvConfig("./", dev);
-
 module.exports = {
   client: "pg",
   connection: env.DATABASE_URL,
