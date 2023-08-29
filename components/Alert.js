46ee6823e5ecff6591ec38167c3e70fd85c5e4a6
Matheus Inocêncio <inolopesm@gmail.com>
2023-08-28 22:23:39 -0300

    add testid datasets on alert component

--- components/Alert.js
+++ components/Alert.js
@@ -1,6 +1,9 @@
 import { ClassNames } from "../utils/ClassNames";
 
-export function Alert({ variant, onClose, children }) {
+export function Alert({ variant, onClose, children, ...props }) {
+  const testId = props["data-testid"];
+  const buttonTestId = props["data-button-testid"];
+
   return (
     <div
       className={
@@ -10,11 +13,14 @@ export function Alert({ variant, onClose, children }) {
           .addIf(variant === "success", "bg-green-50 border-green-300 text-green-800")
           .toString()
       }
+      data-testid={testId}
     >
       <div>{children}</div>
       {onClose && (
         <div className="ml-auto">
-          <button type="button" onClick={onClose}>✖️</button>
+          <button type="button" onClick={onClose} data-testid={buttonTestId}>
+            ✖️
+          </button>
         </div>
       )}
     </div>
