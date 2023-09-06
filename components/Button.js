174bcea847d40593acbd5db395a1ce6402b8d4bd
Matheus Inocêncio <inolopesm@gmail.com>
2023-09-06 18:31:11 -0300

    implement slot on button

--- components/Button.js
+++ components/Button.js
@@ -1,12 +1,16 @@
-export function Button({ type, onClick, disabled, children }) {
+import { Slot } from "@radix-ui/react-slot";
+
+export function Button({ type, onClick, disabled, children, asChild }) {
+  const Component = asChild ? Slot : "button";
+
   return (
-    <button
+    <Component
       className="bg-gray-800 font-medium px-5 py-2.5 rounded text-sm text-white disabled:cursor-not-allowed disabled:bg-gray-400 hover:bg-gray-900"
       type={type}
       onClick={onClick}
       disabled={disabled}
     >
       {children}
-    </button>
+    </Component>
   );
 }
