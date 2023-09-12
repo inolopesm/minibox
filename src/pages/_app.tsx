f15c8880366c0224ee6a0d6b440c3062c3cd7e5a
Matheus Inocêncio <inolopesm@gmail.com>
2023-09-12 17:27:22 -0300

    move custom meta tag to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,17 @@
-import App from "next/app";
+import type { AppProps } from "next/app";
 import "../styles/globals.css";
+import NextHead from "next/head";
 
-export default App;
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <NextHead>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+      </NextHead>
+      <Component {...pageProps} />
+    </>
+  );
+}
