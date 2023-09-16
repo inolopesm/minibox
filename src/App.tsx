791d8dfb9056549500a3238b40754fdc205ba53c
Matheus Inocêncio <inolopesm@gmail.com>
2023-09-16 12:39:46 -0300

    remove next + add vite

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
+
+export function App() {
+  return <RouterProvider router={router} />;
+}
