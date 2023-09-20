f185f294d2324e51c343ff78ad4682dcb7d0b834
Matheus Inocêncio <inolopesm@gmail.com>
2023-09-20 09:20:21 -0300

    lazy route loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,100 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { HomePage } from "./pages/HomePage";
-import { SignInPage } from "./pages/SignInPage";
-import { SignUpPage } from "./pages/SignUpPage";
-import { CreateInvoicePage } from "./pages/invoices/CreateInvoicePage";
-import { InvoicePage } from "./pages/invoices/InvoicePage";
-import { InvoicesPage } from "./pages/invoices/InvoicesPage";
-import { CreatePersonPage } from "./pages/people/CreatePersonPage";
-import { EditPersonPage } from "./pages/people/EditPersonPage";
-import { PeoplePage } from "./pages/people/PeoplePage";
-import { CreateProductPage } from "./pages/products/CreateProductPage";
-import { EditProductPage } from "./pages/products/EditProductPage";
-import { ProductsPage } from "./pages/products/ProductsPage";
-import { CreateTeamPage } from "./pages/teams/CreateTeamPage";
-import { EditTeamPage } from "./pages/teams/EditTeamPage";
-import { TeamsPage } from "./pages/teams/TeamsPage";
 
 const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-  { path: "/signin", element: <SignInPage /> },
-  { path: "/signup", element: <SignUpPage /> },
-  { path: "/products", element: <ProductsPage /> },
-  { path: "/products/create", element: <CreateProductPage /> },
-  { path: "/products/:productId", element: <EditProductPage /> },
-  { path: "/teams", element: <TeamsPage /> },
-  { path: "/teams/create", element: <CreateTeamPage /> },
-  { path: "/teams/:teamId", element: <EditTeamPage /> },
-  { path: "/people", element: <PeoplePage /> },
-  { path: "/people/create", element: <CreatePersonPage /> },
-  { path: "/people/:personId", element: <EditPersonPage /> },
-  { path: "/invoices", element: <InvoicesPage /> },
-  { path: "/invoices/create", element: <CreateInvoicePage /> },
-  { path: "/invoices/:invoiceId", element: <InvoicePage /> },
+  {
+    path: "/",
+    lazy: async () => ({
+      Component: (await import("./pages/HomePage")).HomePage,
+    }),
+  },
+  {
+    path: "/signin",
+    lazy: async () => ({
+      Component: (await import("./pages/SignInPage")).SignInPage,
+    }),
+  },
+  {
+    path: "/signup",
+    lazy: async () => ({
+      Component: (await import("./pages/SignUpPage")).SignUpPage,
+    }),
+  },
+  {
+    path: "/products",
+    lazy: async () => ({
+      Component: (await import("./pages/products/ProductsPage")).ProductsPage,
+    }),
+  },
+  {
+    path: "/products/create",
+    lazy: async () => ({
+      Component: (await import("./pages/products/CreateProductPage"))
+        .CreateProductPage,
+    }),
+  },
+  {
+    path: "/products/:productId",
+    lazy: async () => ({
+      Component: (await import("./pages/products/EditProductPage"))
+        .EditProductPage,
+    }),
+  },
+  {
+    path: "/teams",
+    lazy: async () => ({
+      Component: (await import("./pages/teams/TeamsPage")).TeamsPage,
+    }),
+  },
+  {
+    path: "/teams/create",
+    lazy: async () => ({
+      Component: (await import("./pages/teams/CreateTeamPage")).CreateTeamPage,
+    }),
+  },
+  {
+    path: "/teams/:teamId",
+    lazy: async () => ({
+      Component: (await import("./pages/teams/EditTeamPage")).EditTeamPage,
+    }),
+  },
+  {
+    path: "/people",
+    lazy: async () => ({
+      Component: (await import("./pages/people/PeoplePage")).PeoplePage,
+    }),
+  },
+  {
+    path: "/people/create",
+    lazy: async () => ({
+      Component: (await import("./pages/people/CreatePersonPage"))
+        .CreatePersonPage,
+    }),
+  },
+  {
+    path: "/people/:personId",
+    lazy: async () => ({
+      Component: (await import("./pages/people/EditPersonPage")).EditPersonPage,
+    }),
+  },
+  {
+    path: "/invoices",
+    lazy: async () => ({
+      Component: (await import("./pages/invoices/InvoicesPage")).InvoicesPage,
+    }),
+  },
+  {
+    path: "/invoices/create",
+    lazy: async () => ({
+      Component: (await import("./pages/invoices/CreateInvoicePage"))
+        .CreateInvoicePage,
+    }),
+  },
+  {
+    path: "/invoices/:invoiceId",
+    lazy: async () => ({
+      Component: (await import("./pages/invoices/InvoicePage")).InvoicePage,
+    }),
+  },
 ]);
 
 export function App() {
